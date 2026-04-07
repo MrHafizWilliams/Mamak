58adbda901782fc944a64ae100df1fc349dd61f3
MrHafizWilliams <zul6048@gmail.com>
2026-04-07 14:11:21 +0800

    Update the URL of Mamak

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,14 @@ const server = http.createServer(app);
 // Part 1: Socket.io
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://mamak-seven.vercel.app"], 
+    origin: ["http://localhost:5173", "https://mamak-rho.vercel.app/"], 
     methods: ["GET", "POST"]
   }
 });
 
 // Part 2: Express Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "https://mamak-seven.vercel.app"],
+  origin: ["http://localhost:5173", "https://mamak-rho.vercel.app/"],
   credentials: true
 }));
 app.use(express.json());
